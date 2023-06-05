00996cd8fd0cb865c1420b9488612d19b483e96e
marp-science <67579671+marp-science@users.noreply.github.com>
2023-06-05 14:18:44 +0200

    Update commons.py
    Update train_or_cache

--- commons.py
+++ commons.py
@@ -301,14 +301,12 @@ def prepare_dataset_old(args, augmentation=False):
 
 def original_autoencoder(size=60, kl=False, latentDim=16):
     from pyimagesearch.convautoencoder import ConvAutoencoder
-    print('Przed ConvAE')
+ 
     (encoder, decoder, autoencoder) = ConvAutoencoder.build(size, size, 1, latentDim=latentDim)
-    print('Po ConvAE')
     # opt = tf.keras.optimizers.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
-    # lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=INIT_LR, decay_steps=(INIT_LR / EPOCHS), decay_rate=0.9)
-    # opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
-    opt = tf.keras.optimizers.Adam(learning_rate=INIT_LR)
-    print("Before autoencoder compile!!!")
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=INIT_LR, decay_steps=(INIT_LR / EPOCHS), decay_rate=0.9)
+    opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+    
     autoencoder.compile(loss="mse", optimizer=opt, metrics=['kullback_leibler_divergence' if kl else 'accuracy'])
     return autoencoder
 
@@ -342,7 +340,9 @@ def train_or_cache(train_set, autoencoder, fncache=None, force_train=False, epoc
 
         encoder = Model(autoencoder.input, autoencoder.layers[-2].output)
         encoder.summary()
-        opt = tf.keras.optimizers.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
+        # opt = tf.keras.optimizers.Adam(learning_rate=INIT_LR, decay=INIT_LR / EPOCHS)
+        lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(initial_learning_rate=INIT_LR, decay_steps=(INIT_LR / EPOCHS), decay_rate=0.9)
+        opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
         encoder.compile(loss="mse", optimizer=opt, metrics=['accuracy'])
         encoder.save(fn.replace('.h5', '-encoder.h5'))
 
